2850369763658b44371d35db09c0562c536468c6
MrDen1611 <52958464+MrDen1611@users.noreply.github.com>
2021-11-27 13:53:40 +0000

    lab26

--- src/courses/dmahov/v8/task2/Subtask1.java
+++ src/courses/dmahov/v8/task2/Subtask1.java
@@ -5,53 +5,65 @@ public class Subtask1 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("Какое задание вы хотите запустить?");
+        System.out.println("1. 8");
+        System.out.println("2. 10");
+        System.out.print("Введите число: ");
+        double vv = in.nextDouble();
+        if (vv == 1) {
+            num8();
+        }
+        else if (vv == 2) {
+            num10();
+        }
+    }
+
+    public static void num8() {
+        Scanner in = new Scanner(System.in);
         System.out.print("Введите число К: ");
         double k1 = in.nextDouble();
         System.out.print("Введите число L: ");
         double l1 = in.nextDouble();
         System.out.print("Введите число M: ");
         double m1 = in.nextDouble();
-        int out1 = 0;
         int out2 = 0;
         boolean k2 = false;
         boolean l2 = false;
         boolean m2 = false;
         double out3 = 0;
         if ( k1 % 2 == 0 ) {
-            out2 = out1 + 1;
+            out2 = out2 + 1;
             k2 = true;
         }
         if (l1 % 2 == 0) {
-            out2 = out1 + 1;
+            out2 = out2 + 1;
             l2 = true;
         }
 
         if (m1 % 2 == 0) {
-            out2 = out1 + 1;
+            out2 = out2 + 1;
             m2 = true;
         }
+
         int out4 = 0;
         if (k2) {
             out3 = out3 + k1;
-            out4 = out4+1;
-        }
-        if (l2) {
+            out4 = out4 + 1;
+        } if (l2) {
             out3 = out3 + l1;
             out4 = out4+1;
-        }
-        if (m2) {
+        } if (m2) {
             out3 = out3 + m1;
             out4 = out4+1;
         }
+
         double out5 = out3 / out4;
         System.out.print("Количество чётных чисел: ");
         System.out.println(out2);
 
         System.out.print("Среднеарифметическое положительных чисел: ");
         System.out.println(out5);
-        num10();
     }
-
     public static void num10() {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите x координату точки m1: ");
